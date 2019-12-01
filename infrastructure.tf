1df1c5e22e91d71c03445f6d5d5f441bb1ef6424
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2019-12-01 22:48:36 +0000

    update

--- infrastructure.tf
+++ infrastructure.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "game_security_group" {
 
 # TODO Comment 2-3 sentences.
 resource "aws_instance" "game_server" {
-  ami                    = "ami-04763b3055de4860b"
+  ami                    = "ami-04b9e92b5572fa0d1"
   instance_type          = "t2.micro"
   key_name               = "GameKeyPair"
   vpc_security_group_ids = [aws_security_group.game_security_group.id]
