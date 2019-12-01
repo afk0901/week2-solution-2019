86dd25ae4607ef00aab62a6921665b545c263214
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2019-12-01 19:06:57 +0000

    update

--- infrastructure.tf
+++ infrastructure.tf
@@ -54,14 +54,16 @@ resource "aws_instance" "game_server" {
     }
   }
 
+  # TODO Comment 1-2 sentences.
   provisioner "file" {
     source      = "scripts/docker_compose_up.sh"
     destination = "/home/ubuntu/docker_compose_up.sh"
 
     connection {
+      host        = coalesce(self.public_ip, self.private_ip)
       type        = "ssh"
       user        = "ubuntu"
-      private_key = "${file("~/.aws/GameKeyPair.pem")}"
+      private_key = file("~/.aws/GameKeyPair.pem")
     }
   }
 
