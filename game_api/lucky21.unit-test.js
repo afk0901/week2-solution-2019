b1ced05de93e19fcc0b66caa91e974cc43de2a09
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2019-12-01 16:11:41 +0000

    fix lints

--- game_api/lucky21.unit-test.js
+++ game_api/lucky21.unit-test.js
@@ -3,12 +3,12 @@ const dealerConstructor = require('./dealer.js');
 const lucky21Constructor = require('./lucky21.js');
 
 test('a new game should have 50 cards left in the deck', () => {
-  let game = lucky21Constructor(deckConstructor(), dealerConstructor());
+  const game = lucky21Constructor(deckConstructor(), dealerConstructor());
   expect(game.state.deck.length).toEqual(50);
 });
 
 test('a new game should have 2 drawn cards', () => {
-  let game = lucky21Constructor(deckConstructor(), dealerConstructor());
+  const game = lucky21Constructor(deckConstructor(), dealerConstructor());
   expect(game.state.cards.length).toEqual(2);
 });
 
@@ -16,18 +16,18 @@ test('guess21OrUnder should draw the next card', () => {
   // Arrange
   let deck = deckConstructor();
   deck = [
-      '05C', '01D', '09S', '10H', 
+    '05C', '01D', '09S', '10H',
   ];
-  let dealer = dealerConstructor();
+  const dealer = dealerConstructor();
   // Override the shuffle to do nothing.
   dealer.shuffle = (deck) => {};
-  
+
   // Inject our dependencies
-  let game = lucky21Constructor(deck, dealer);
-  
+  const game = lucky21Constructor(deck, dealer);
+
   // Act
   game.guess21OrUnder(game);
-  
+
   // Assert
   expect(game.state.cards.length).toEqual(3);
   expect(game.state.cards[2]).toEqual('01D');
