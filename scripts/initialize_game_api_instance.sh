ad91f898a92c5fb17c78b2d4bb975fcb5551f099
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2019-12-01 21:34:58 +0000

    update

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -4,20 +4,12 @@ echo 'This script installs everything needed to run our API on the instance'
 echo 'and then starts the API.'
 
 echo 'Installing Docker'
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker ubuntu
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker $USER
 
 echo 'Install Docker Compose'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
