992f8c1a5b43428f645a975ac6841677a54048c9
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2019-12-01 16:53:13 +0000

    update

--- game_api/lucky21.js
+++ game_api/lucky21.js
@@ -13,41 +13,71 @@ module.exports = (deck, dealer) => {
     card: undefined,
   };
   return {
-    state: state,
-    // Is the game over (true or false).
+    state: {
+      deck: deck,
+      dealer: dealer,
+      cards: [card0, card1],
+      card: undefined,
+    },
     isGameOver: (game) => {
-      // TODO
+      return game.state.card !== undefined
+          || game.getTotal(game) >= 21;
     },
-    // Has the player won (true or false).
     playerWon: (game) => {
-      // TODO
+      return (game.state.card !== undefined && game.getTotal(game) > 21)
+          || game.getCardsValue(game) == 21;
     },
-    // The highest score the cards can yield without going over 21 (integer).
     getCardsValue: (game) => {
-      // TODO
+      let cardsValue = 0;
+
+      // count everything and treat all aces as 1.
+      for (let i = 0; i < game.state.cards.length; i++) {
+        const cardValue = parseInt(game.state.cards[i].substring(0, 2));
+        cardsValue += Math.min(cardValue, 10);
+      }
+
+      // foreach ace check if we can add 10.
+      for (let i = 0; i < game.state.cards.length; i++) {
+        const cardValue = parseInt(game.state.cards[i].substring(0, 2));
+        if (cardValue == 1) {
+          if (cardsValue + 10 <= 21) {
+            cardsValue += 10;
+          }
+        }
+      }
+
+      return cardsValue;
     },
-    // The value of the card that should exceed 21 if it exists (integer or undefined).
     getCardValue: (game) => {
-      // TODO
+      const card = game.state.card;
+      if (card === undefined) return card;
+
+      const cardValue = parseInt(card.substring(0, 2));
+      return Math.min(cardValue, 10);
     },
     getTotal: (game) => {
-      // TODO
+      return game.getCardsValue(game) + (game.getCardValue(game) || 0);
     },
-    // The player's cards (array of strings).
-    getCards: (game) => {
-      // TODO
-    },
-    // The player's card (string or undefined).
-    getCard: (game) => {
-      // TODO
-    },
-    // Player action (void).
+    getCards: (game) => game.state.cards,
+    getCard: (game) => game.state.card,
     guess21OrUnder: (game) => {
-      // TODO
+      const nextCard = dealer.draw(game.state.deck);
+      game.state.cards.push(nextCard);
     },
-    // Player action (void).
     guessOver21: (game) => {
-      // TODO
+      const nextCard = dealer.draw(game.state.deck);
+      game.state.card = nextCard;
+    },
+    getState: (game) => {
+      return {
+        cards: game.state.cards,
+        cardsValue: game.getCardsValue(game),
+        card: game.state.card,
+        cardValue: game.getCardValue(game),
+        total: game.getTotal(game),
+        gameOver: game.isGameOver(game),
+        playerWon: game.playerWon(game),
+      };
     },
   };
 };
