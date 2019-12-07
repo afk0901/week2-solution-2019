6e0fdfb834c12404520436a82255ba95269ae6f1
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2019-12-07 20:13:00 +0000

    fix lints

--- game_api/deck.unit-test.js
+++ game_api/deck.unit-test.js
@@ -2,28 +2,28 @@ const deckConstructor = require('./deck.js');
 
 // Deck tests
 test('The Deck should contain 52 cards', () => {
-    // Arange
-    let deck = deckConstructor();
+  // Arange
+  const deck = deckConstructor();
 
-    // Act
-    // Assert
-    expect(deck.length).toEqual(52);
+  // Act
+  // Assert
+  expect(deck.length).toEqual(52);
 });
 
 test('The first card in the deck should be 01H', () => {
-    // Arange
-    let deck = deckConstructor();
+  // Arange
+  const deck = deckConstructor();
 
-    // Act
-    // Assert
-    expect(deck[0]).toEqual('01H');
+  // Act
+  // Assert
+  expect(deck[0]).toEqual('01H');
 });
 
 test('The last card in the deck should be 01H', () => {
-    // Arange
-    let deck = deckConstructor();
+  // Arange
+  const deck = deckConstructor();
 
-    // Act
-    // Assert
-    expect(deck[51]).toEqual('13S');
+  // Act
+  // Assert
+  expect(deck[51]).toEqual('13S');
 });
--- game_api/random.unit-test.js
+++ game_api/random.unit-test.js
@@ -1,38 +1,38 @@
 // Random value min test
 test('random should return an int that is more than or equal to the min value parameter for randomInt()', () => {
-    // Arrange
-    let random = require('./random')();
+  // Arrange
+  const random = require('./random')();
 
-    // Act
-    let number = random.randomInt(5, 10);
+  // Act
+  const number = random.randomInt(5, 10);
 
-    // Assert
-    expect(number).toBeGreaterThanOrEqual(5);
+  // Assert
+  expect(number).toBeGreaterThanOrEqual(5);
 });
 
 // Random value max test
 test('random should return an int that is less than or equal to the max value parameter for randomInt()', () => {
-    // Arrange
-    let random = require('./random')();
+  // Arrange
+  const random = require('./random')();
 
-    // Act
-    let number = random.randomInt(0, 5);
+  // Act
+  const number = random.randomInt(0, 5);
 
-    // Assert
-    expect(number).toBeLessThanOrEqual(5);
+  // Assert
+  expect(number).toBeLessThanOrEqual(5);
 });
 
 // Random override test
 test('random should return a value of 1 after overriding the randomInt() fucntion', () => {
-    // Arrange
-    let random = require('./random')();
-    random.randomInt = (min, max) => {
-        return 1;
-    };
+  // Arrange
+  const random = require('./random')();
+  random.randomInt = (min, max) => {
+    return 1;
+  };
 
-    // Act
-    let number = random.randomInt(1, 2);
+  // Act
+  const number = random.randomInt(1, 2);
 
-    // Assert
-    expect(number).toEqual(1);
+  // Assert
+  expect(number).toEqual(1);
 });
